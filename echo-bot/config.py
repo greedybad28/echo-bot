14cf7d7b63179c25528e67f7ee58fee2e8ea41e5
REmoo <remoantony32@gmail.com>
2024-02-05 01:52:59 +0530

    Finished off config.py

--- echo-bot/config.py
+++ echo-bot/config.py
@@ -1,3 +1,16 @@
 import sys
+import json
 path ="config/config.json"
-print(sys.argv[1])
+print(sys.argv[0])
+
+with open(path,"r") as stream:
+    config=json.load(stream)
+
+print(config)
+BOT_TOKEN = config["bot_token"]
+DB_PATH = config["db_path"]
+ADMINS = config["admins"]
+
+print(BOT_TOKEN,DB_PATH,ADMINS)
+
+## LATER INCLUDE except filenotfound error...
