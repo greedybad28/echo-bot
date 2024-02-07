e23226050c27d4f9c986ffee0cc4e73d87cca90e
REmoo <remoantony32@gmail.com>
2024-02-07 16:10:50 +0530

    ..

--- echo-bot/main.py
+++ echo-bot/main.py
@@ -2,8 +2,6 @@ from typing import Final
 from telegram import Update
 from telegram.ext import Application,CommandHandler,MessageHandler,filters,ContextTypes
 import asyncio
-import config
-from config import BOT_TOKEN,DB_PATH,ADMINS
+from config import BOT_TOKEN,DB_PATH
 
-print(BOT_TOKEN)
 
